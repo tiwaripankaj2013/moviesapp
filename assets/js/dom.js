138de47418f24f0c990888ef72471baad98a5a3b
tiwaripankaj20013 <tiwaripankaj20013@users.noreply.github.com>
2019-10-30 16:39:38 +0530

    movies app js update

--- assets/js/dom.js
+++
@@ -1,4 +0,0 @@
-import {movieCard,rowMarkup} from './create-html.js';
-const POSTER_PATH_PREFIX =`https://image.tmdb.org/t/p/w500/`;
- 
-
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,5 @@
 
 import { getGenres, getLatestMovies, getTrendingMovies, getPopularMovies } from './movies-api.js';
 import { createRow } from './dom.js';
-// create iife to call latest,trending,popular movies function;
-(()=>{
-    //create moviesData array store all server data 
-    let moviesData = [];
-    let genre;
-    async function  getData(){
-        genre = await getGenres();
-        moviesData.push = (await getLatestMovies());
-        moviesData.push = (await getTrendingMovies());
-        moviesData.push = (await getPopularMovies());
-    }
-    
-    async function hero(){
-        await getData();
-        moviesData.forEach((ele,idx)=>{
-            createRow(ele.results,idx,genre.genres);
-        });
-    }
-    hero();
-console.log('welcome to js');
-})();
+ 
+ 
